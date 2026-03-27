1c35f327fde636bf104ca69ce8ff0f04254aa10e
ikosmaz <77364521+ikosmaz@users.noreply.github.com>
2026-03-27 18:21:52 +0100

    Update email configuration in wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -21,9 +21,9 @@ os.environ['DJANGO_EMAIL_BACKEND'] = 'django.core.mail.backends.smtp.EmailBacken
 os.environ['DJANGO_EMAIL_HOST'] = 'smtp.gmail.com'
 os.environ['DJANGO_EMAIL_PORT'] = '587'
 os.environ['DJANGO_EMAIL_USE_TLS'] = '1'
-os.environ['DJANGO_EMAIL_HOST_USER'] = 'kosmazisa@gmail.com'
-os.environ['DJANGO_EMAIL_HOST_PASSWORD'] = 'fdhj wzjo afrp dskp'
-os.environ['DJANGO_DEFAULT_FROM_EMAIL'] = 'kosmazisa@gmail.com'
+os.environ['DJANGO_EMAIL_HOST_USER'] = 'email@gmail.com'
+os.environ['DJANGO_EMAIL_HOST_PASSWORD'] = '16 char app pass' """Get code from gmail, it is not email password"""
+os.environ['DJANGO_DEFAULT_FROM_EMAIL'] = 'email@gmail.com'
 
 
 application = get_wsgi_application()
